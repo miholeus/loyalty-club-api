15756b601d74d5fd023b135ac13727af6c9c4240
Maxim Parfenov <maxim@selloutsport.com>
2017-09-04 11:44:01 +0300

    refs AFR-1168 #time 1h
    #comment Создать сущность и миграцию для таблицу UserReferralActivation

--- src/Zenomania/CoreBundle/Entity/UserReferralActivation.php
+++ src/Zenomania/CoreBundle/Entity/UserReferralActivation.php
@@ -14,4 +14,96 @@ class UserReferralActivation
 
     /** @var  */
     private $id;
+
+    /** @var \DateTime */
+    private $date;
+
+    /** @var \Zenomania\CoreBundle\Entity\UserReferralCode */
+    private $refCode;
+
+    /** @var \Zenomania\CoreBundle\Entity\User */
+    private $createdByUser;
+
+    /** @var \Zenomania\CoreBundle\Entity\User */
+    private $usedByUser;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate(): \DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return UserReferralCode
+     */
+    public function getRefCode(): UserReferralCode
+    {
+        return $this->refCode;
+    }
+
+    /**
+     * @param UserReferralCode $refCode
+     */
+    public function setRefCode(UserReferralCode $refCode)
+    {
+        $this->refCode = $refCode;
+    }
+
+    /**
+     * @return User
+     */
+    public function getCreatedByUser(): User
+    {
+        return $this->createdByUser;
+    }
+
+    /**
+     * @param User $createdByUser
+     */
+    public function setCreatedByUser(User $createdByUser)
+    {
+        $this->createdByUser = $createdByUser;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUsedByUser(): User
+    {
+        return $this->usedByUser;
+    }
+
+    /**
+     * @param User $usedByUser
+     */
+    public function setUsedByUser(User $usedByUser)
+    {
+        $this->usedByUser = $usedByUser;
+    }
 }
