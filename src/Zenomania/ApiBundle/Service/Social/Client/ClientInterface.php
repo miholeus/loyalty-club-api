eb6145e78964a7677d1af985aa2edd51387735e1
Igor Volkov <i.volkov@selloutsport.com>
2017-09-08 17:40:30 +0300

    add guzzle client

---
+++ src/Zenomania/ApiBundle/Service/Social/Client/ClientInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 07.09.17
+ * Time: 16:05
+ */
+
+namespace Zenomania\ApiBundle\Service\Social\Client;
+
+
+interface ClientInterface
+{
+
+}
---
+++ src/Zenomania/ApiBundle/Service/Social/Client/VkontakteClient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 07.09.17
+ * Time: 16:04
+ */
+
+namespace Zenomania\ApiBundle\Service\Social\Client;
+
+
+class VkontakteClient
+{
+
+}
