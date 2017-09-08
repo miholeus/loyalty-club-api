941e0705af8c031e19f1b2294e1a839c0ccc8c1f
Igor Volkov <i.vollov@selloutsport.com>
2017-09-08 18:33:50 +0300

    add http client

---
+++ src/Zenomania/ApiBundle/Service/Social/Client/VkontakteClient.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 07.09.17
+ * Time: 16:04
+ */
+
+namespace Zenomania\ApiBundle\Service\Social\Client;
+
+
+use Zenomania\ApiBundle\Form\Model\ProfileSocialData;
+use GuzzleHttp\Client;
+
+class VkontakteClient implements ClientInterface
+{
+    protected $version = 5.68;
+
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client(
+            array('base_uri' => 'https://api.vk.com/method/')
+        );
+    }
+
+    /**
+     * Gets profile info from vkontakte
+     *
+     * @param ProfileSocialData $socialData
+     * @return mixed
+     */
+    public function getProfile(ProfileSocialData $socialData)
+    {
+        $queryData = array(
+            'user_id' => $socialData->getUserId(),
+            'access_token' => $socialData->getAccessToken(),
+            'v' => $this->version,
+        );
+        $response = $this->client->request(
+            'GET',
+            'account.getProfileInfo',
+            array('query' => $queryData)
+        );
+        $userInfo = \GuzzleHttp\json_decode($response->getBody()->getContents());
+        if (isset($userInfo->response)) {
+            $userInfo = $userInfo->response;
+            var_dump($userInfo);
+            return $userInfo;
+        }
+        return false;
+    }
+}
