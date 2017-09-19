b0a363cbe3fc85019daa95a95d41690dabf43ba8
Maxim Parfenov <maxim@selloutsport.com>
2017-09-19 13:39:30 +0300

    AFR-1523 #time 10m
    #comment Поменять тип данных с varchar(256) на varchar(255)

--- app/DoctrineMigrations/Version20170915130417.php
+++ app/DoctrineMigrations/Version20170915130417.php
@@ -18,7 +18,7 @@ class Version20170915130417 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE device_token (id SERIAL NOT NULL, user_id INT DEFAULT NULL, token VARCHAR(256) DEFAULT NULL, created_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, device_type VARCHAR(256) DEFAULT NULL, device_name VARCHAR(256) DEFAULT NULL, status INT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE device_token (id SERIAL NOT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) DEFAULT NULL, created_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, device_type VARCHAR(255) DEFAULT NULL, device_name VARCHAR(255) DEFAULT NULL, status INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX device_token__user_id ON device_token (user_id)');
         $this->addSql('ALTER TABLE device_token ADD CONSTRAINT FK_99B2415CA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
