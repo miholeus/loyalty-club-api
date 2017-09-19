770cb71ad01ff0dd0ebaa97a3a9374779e0e3634
Oleg Mikhailov <mikhailov@selloutsport.com>
2017-09-19 19:09:22 +0300

    AFR-1468 #time 1h 30m #comment added datetime range

--- src/Zenomania/CoreBundle/Resources/public/plugins/datepicker/settings.js
+++ src/Zenomania/CoreBundle/Resources/public/plugins/datepicker/settings.js
@@ -1,22 +1,34 @@
-$('.datepicker-day').datepicker({
-	format: "dd.mm.yyyy",
-	language: "ru",
-	multidate: false,
-	autoclose: true
-});
+if (jQuery().datepicker) {
+    $('.datepicker-day').datepicker({
+        format: "dd.mm.yyyy",
+        language: "ru",
+        multidate: false,
+        autoclose: true
+    });
 
-$('.datepicker-month').datepicker({
-	format: "dd.mm.yyyy",
-	language: "ru",
-	multidate: false,
-	autoclose: true,
-	minViewMode: 1
-});
+    $('.datepicker-month').datepicker({
+        format: "dd.mm.yyyy",
+        language: "ru",
+        multidate: false,
+        autoclose: true,
+        minViewMode: 1
+    });
 
-$('.datepicker-year').datepicker({
-	format: "dd.mm.yyyy",
-	language: "ru",
-	multidate: false,
-	autoclose: true,
-	minViewMode: 2
-});
+    $('.datepicker-year').datepicker({
+        format: "dd.mm.yyyy",
+        language: "ru",
+        multidate: false,
+        autoclose: true,
+        minViewMode: 2
+    });
+}
+
+if (jQuery().datetimepicker) {
+    $('.datetimepicker').datetimepicker({
+        format: "dd.mm.yyyy hh:ii:ss",
+        language: "ru",
+        multidate: false,
+        autoclose: true,
+        minViewMode: 1
+    });
+}
