b43aa7eff6373545f9647ec24e3a6cbc533b52ed
Igor Volkov <i.volkov@selloutsport.com>
2017-09-27 15:09:03 +0300

    AFR-1536 #time 4h #comment create tables badge & badge_type

---
+++ src/Zenomania/CoreBundle/Entity/Badge.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace Zenomania\CoreBundle\Entity;
+
+/**
+ * Badge
+ */
+class Badge
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var string
+     */
+    private $photo;
+
+    /**
+     * @var BadgeType
+     */
+    private $typeId;
+
+    /**
+     * @var int
+     */
+    private $sort;
+
+    /**
+     * @var int
+     */
+    private $points;
+
+    /**
+     * @var int
+     */
+    private $maxPoints;
+
+    /**
+     * @var bool
+     */
+    private $active;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Badge
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Badge
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     *
+     * @return Badge
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set typeId
+     *
+     * @param BadgeType $typeId
+     *
+     * @return Badge
+     */
+    public function setTypeId($typeId)
+    {
+        $this->typeId = $typeId;
+
+        return $this;
+    }
+
+    /**
+     * Get typeId
+     *
+     * @return BadgeType
+     */
+    public function getTypeId()
+    {
+        return $this->typeId;
+    }
+
+    /**
+     * Set sort
+     *
+     * @param integer $sort
+     *
+     * @return Badge
+     */
+    public function setSort($sort)
+    {
+        $this->sort = $sort;
+
+        return $this;
+    }
+
+    /**
+     * Get sort
+     *
+     * @return int
+     */
+    public function getSort()
+    {
+        return $this->sort;
+    }
+
+    /**
+     * Set points
+     *
+     * @param integer $points
+     *
+     * @return Badge
+     */
+    public function setPoints($points)
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    /**
+     * Get points
+     *
+     * @return int
+     */
+    public function getPoints()
+    {
+        return $this->points;
+    }
+
+    /**
+     * Set maxPoints
+     *
+     * @param integer $maxPoints
+     *
+     * @return Badge
+     */
+    public function setMaxPoints($maxPoints)
+    {
+        $this->maxPoints = $maxPoints;
+
+        return $this;
+    }
+
+    /**
+     * Get maxPoints
+     *
+     * @return int
+     */
+    public function getMaxPoints()
+    {
+        return $this->maxPoints;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     *
+     * @return Badge
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return bool
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
+
