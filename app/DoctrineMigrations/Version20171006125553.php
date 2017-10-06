7e981f084da8d8b31706f662207cf62d3f42fcea
Maxim Parfenov <maxim@selloutsport.com>
2017-10-06 16:00:01 +0300

    AFR-1583 #time 40m
    #comment Создать таблицу для хранения данных для типа и количества начисляемых баллов

---
+++ app/DoctrineMigrations/Version20171006125553.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zenomania\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171006125553 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE points_type (id SERIAL NOT NULL, type VARCHAR(255) NOT NULL, interval SMALLINT DEFAULT 0 NOT NULL, percent SMALLINT DEFAULT 0 NOT NULL, is_active BOOLEAN DEFAULT NULL, created_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE points_type');
+    }
+}
