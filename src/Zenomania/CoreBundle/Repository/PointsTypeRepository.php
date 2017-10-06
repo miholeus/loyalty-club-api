694040e692b2fcd5742ed1de9901597b4f0384a0
Maxim Parfenov <maxim@selloutsport.com>
2017-10-06 16:19:16 +0300

    AFR-1583 #time 40m
    #comment Создать репозиторий для работы с entity PointsType. Создать метод для поиска количества процентов, которые надо начислить в зависимости от времени прохода на стадион и типа билета

---
+++ src/Zenomania/CoreBundle/Repository/PointsTypeRepository.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author  Gizmo <gizmolife@gmail.com> {@link https://vk.com/id3558208}
+ * Date: 06.10.2017
+ * Time: 16:01
+ */
+
+namespace Zenomania\CoreBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class PointsTypeRepository extends EntityRepository
+{
+
+    /**
+     * Найти какое количество % соответствует данному типу и интервалу времени
+     *
+     * @param string $type
+     * @param int $interval
+     * @return mixed
+     */
+    public function findPercentByTypeAndInterval(string $type, integer $interval)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $query = $qb->select('pt.percent')
+            ->from('ZenomaniaCoreBundle:PointsType', 'pt')
+            ->where('pt.type = :type')
+            ->andWhere('pt.interval >= :interval')
+            ->setParameter('type', $type)
+            ->setParameter('interval', $interval)
+            ->orderBy('pt.interval', 'ASC')
+            ->setMaxResults(1)
+            ->getQuery();
+
+        return $query->getSingleScalarResult();
+    }
+}
