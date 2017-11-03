1ff77f43585dc56c4fa3ebcb2dd97191bc161958
Oleg Mikhailov <mikhailov@selloutsport.com>
2017-11-03 16:22:08 +0300

    fix repost command

--- src/Zenomania/CoreBundle/Command/CheckRepostCommand.php
+++ src/Zenomania/CoreBundle/Command/CheckRepostCommand.php
@@ -12,7 +12,6 @@ namespace Zenomania\CoreBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Zenomania\CoreBundle\Entity\User;
 
 class CheckRepostCommand extends ContainerAwareCommand
 {
