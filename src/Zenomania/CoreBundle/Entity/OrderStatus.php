6fc0ee2e65e67463e9c2b844abac110ad1b35dc9
Igor Volkov <i.volkov@selloutsport.com>
2017-11-07 15:47:31 +0300

    AFR-1620 #time 30m #comment create orderStatus entity

---
+++ src/Zenomania/CoreBundle/Entity/OrderStatus.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Zenomania\CoreBundle\Entity;
+
+/**
+ * OrderStatus
+ */
+class OrderStatus
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $code;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return OrderStatus
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set code.
+     *
+     * @param string $code
+     *
+     * @return OrderStatus
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code.
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+}
