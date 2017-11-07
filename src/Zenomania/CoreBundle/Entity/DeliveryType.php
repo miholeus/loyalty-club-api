91b255f239a423cd262a3a68d632b2c29007042f
Igor Volkov <i.volkov@selloutsport.com>
2017-11-07 15:49:46 +0300

    AFR-1620 #time 30m #comment create deliveryType entity

---
+++ src/Zenomania/CoreBundle/Entity/DeliveryType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Zenomania\CoreBundle\Entity;
+
+/**
+ * DeliveryType
+ */
+class DeliveryType
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return DeliveryType
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
