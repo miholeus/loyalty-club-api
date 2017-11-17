4672a9c62e8ebbb276b3b8de1b2f184c060f8116
Oleg Mikhailov <mikhailov@selloutsport.com>
2017-11-17 11:59:08 +0300

    AFR-1646 #time 3h #comment сохранение клубов

---
+++ src/Zenomania/CoreBundle/Event/Listener/ClubListener.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package    Zenomania\CoreBundle\Event\Listener
+ * @author     miholeus <me@miholeus.com> {@link http://miholeus.com}
+ */
+
+
+namespace Zenomania\CoreBundle\Event\Listener;
+
+
+use Zenomania\CoreBundle\Event\Club\ClubReceivedEvent;
+
+class ClubListener
+{
+    public function onClubReceivedEvent(ClubReceivedEvent $event)
+    {
+
+    }
+}
