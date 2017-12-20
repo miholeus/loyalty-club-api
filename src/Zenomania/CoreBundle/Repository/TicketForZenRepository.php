4eeacee22e6ca1e0ce99a61cc88009b64a2edc5f
Unknown <i.volkov@selloutsport.com>
2017-12-20 23:49:53 +0300

    AFR-1676 #comment связываем билет с юзером

---
+++ src/Zenomania/CoreBundle/Repository/TicketForZenRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author  Gizmo <gizmolife@gmail.com> {@link https://vk.com/id3558208}
+ * Date: 15.08.2017
+ * Time: 12:26
+ */
+
+namespace Zenomania\CoreBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class TicketRepository extends EntityRepository
+{
+
+}
