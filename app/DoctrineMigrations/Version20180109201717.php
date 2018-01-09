be89f954b64fc87cb97b031593192a3bd8408b0a
Oleg Mikhailov <mikhailov@selloutsport.com>
2018-01-09 23:18:54 +0300

    fixed points type interval

---
+++ app/DoctrineMigrations/Version20180109201717.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Zenomania\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180109201717 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE points_type ALTER "interval" TYPE INT');
+        $this->addSql('ALTER TABLE points_type ALTER "interval" SET DEFAULT 0');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE points_type ALTER interval TYPE SMALLINT');
+        $this->addSql('ALTER TABLE points_type ALTER interval SET DEFAULT 0');
+    }
+}
